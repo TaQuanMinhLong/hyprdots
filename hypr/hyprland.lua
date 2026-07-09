03c07ce4528adad7f31919e49f51ddabd9c3c223
TaQuanMinhLong <tqml2013@gmail.com>
2026-07-09 17:58:18 +0700

    remove GTK_IM_MODULE from hyprland env (fcitx Wayland warning)

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -65,7 +65,6 @@ end)
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
-hl.env("GTK_IM_MODULE", "fcitx")
 hl.env("QT_IM_MODULE", "fcitx")
 hl.env("XMODIFIERS", "@im=fcitx")
 hl.env("INPUT_METHOD", "fcitx")
